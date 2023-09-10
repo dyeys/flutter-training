6af2203e4ec433b28f57b877f568641ed3ea6056
dyeys <dyeys23@gmail.com>
2023-09-10 20:28:02 +0800

    updated as of 9pm sept 9

--- lib/main.dart
+++ lib/main.dart
@@ -16,17 +16,37 @@ void main() {
         backgroundColor: Color.fromARGB(255, 70, 68, 151),
          body: Center(
           child: Text('To Do: ', 
-          style: TextStyle(fontSize: 30,
+          style: TextStyle(fontSize: 40,
           color: Colors.white
-          ),
-
+            ),
           ),
           
-
         ),
-        
+          
+
+        floatingActionButton: FloatingActionButton(
+          elevation: 20.0,
+          child: const Icon(Icons.add),
+          onPressed: (){
+          }
+        ),       
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 0,
+          items: [
+            BottomNavigationBarItem(
+              label: "Home",
+              icon: Icon(Icons.home),
+            ),
+              BottomNavigationBarItem(
+            label: "Search",
+            icon: Icon(Icons.search),
+          ),
+          ],
+          onTap: (int indexOfItem) {},
+        ),
+ 
+
       ),
-      
     );
 
   }
