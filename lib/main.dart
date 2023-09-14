c7f625b839400bce33a987b5d32675a5e9c824da
dyeys <dyeys23@gmail.com>
2023-09-14 21:53:57 +0800

    displaying input from textfield :(

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
- class MyApp extends StatefulWidget {
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
+
 class _MyAppState extends State<MyApp> {
   //get onPressed => null;
 
@@ -22,56 +24,56 @@ class _MyAppState extends State<MyApp> {
       title: 'To-Do List',
       home: Scaffold(
         backgroundColor: Color.fromARGB(255, 146, 209, 254),
-         body: Column(
+        body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SizedBox(
               height: 100.0,
               width: 500.0,
               child: Center(
-              child: Text('TODOAPP',
-                style: TextStyle(fontSize: 40.0),
+                child: Text(
+                  'TODOAPP',
+                  style: TextStyle(fontSize: 40.0),
+                ),
               ),
-            ),
             ),
             SizedBox(
               height: 100.0,
               width: 350.0,
               child: Center(
-              child: TextField(
-                obscureText: false,
+                child: TextField(
+                  obscureText: false,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
-                    labelText: 'Input Here',                      
-                          ),  
-                        ),
-                      ),
-                    ),
+                    labelText: samp,
+                  ),
+                ),
+              ),
+            ),
             SizedBox(
               height: 50.0,
               width: 200.0,
-              //child: ElevatedButton(onPressed: onPressed, 
-                child: Text('SAVE',
-                  style: TextStyle(fontSize: 20.0),
-              ),
+              child: Center(
+                  child: Column(
+                children: [
+                  ElevatedButton(
+                    child: Text('SUBMIT'),
+                    onPressed: () {
+                      //samp = 'Hello!';
+                      print(samp);
+                    },
+                  ),
+                ],
+              )),
             ),
-            //),
-            Center(
-	            child:Column(
-            	  children:[
-	                ElevatedButton(
-		                child:Text('Test'),
-		                onPressed:() {
-		                  samp = 'my text';
-		                  print(samp);
-		            },	
+            SizedBox(
+              height: 150.0,
+              width: 200.0,
+              child: Text(sam + samp),
             ),
-         ],
-         )
-        ),       
-        ],
+          ],
         ),
-        ),
+      ),
     );
   }
 }
